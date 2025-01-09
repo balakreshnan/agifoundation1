6582ec3ac4eae41fcbd7fe6b01e5e3a416642192
balakreshnan <babal@microsoft.com>
2025-01-09 08:27:20 -0600

    updated

--- mfgagentst.py
+++ mfgagentst.py
@@ -5,7 +5,7 @@ from autogen_agentchat.conditions import MaxMessageTermination, TextMentionTermi
 from autogen_agentchat.messages import AgentEvent, ChatMessage
 from autogen_agentchat.teams import SelectorGroupChat
 from autogen_agentchat.ui import Console
-from autogen_ext.models.openai import OpenAIChatCompletionClient, AzureOpenAIChatCompletionClient
+from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
 import asyncio
 import os
 from openai import AzureOpenAI
